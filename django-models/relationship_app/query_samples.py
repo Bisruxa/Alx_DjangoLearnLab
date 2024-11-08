fe5ed8eb37e44b856d707c238da4e6aee3489852
bisruxa <imbisru@gmail.com>
2024-11-08 11:05:42 -0900

    shell command added

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,6 +1,6 @@
 from .models import Book,Library,Librarian 
 book = Book.objects.get(author = "")
-library = Library.objects.get(name = "Library Name")
+library = Library.objects.get(name = "library_name")
 books = library.book.all()
 for book in books:
   print(book)
