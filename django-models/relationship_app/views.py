f783191f6933a9f4ba1476a950e627e59263102a
bisruxa <imbisru@gmail.com>
2024-11-08 12:58:01 -0900

    updated

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,7 +7,7 @@ def storage(request):
  return render(request, 'relationship_app/list_books.html', {'book': books})
 class viewss(DetailView):
   model = Book
-  template_name = 'library_detail.html'
+  template_name = 'relationship_app/library_detail.html'
   def get_contex_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
     library = self.objects.all()
