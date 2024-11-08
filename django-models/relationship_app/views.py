e2cdcac1d9905149e6df6e61a04e930d14a0db3c
bisruxa <imbisru@gmail.com>
2024-11-08 12:58:50 -0900

    updated

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Author, Library, Librarian
+from .models import Book, Author, Librarian
+from .models import Library
 
 def storage(request):
  books =Book.objects.all()
