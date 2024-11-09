df129f40ca2eb6b8485d027e2036b280a72b48c8
bisruxa <imbisru@gmail.com>
2024-11-09 09:45:21 -0900

    commited

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,14 @@
+# from django.urls import path
+# from .views import list_books, LibraryDetailView
+# urlpatterns = [
+#   path('', list_books, name='index'),
+#   path('detail/<int:book_id>', LibraryDetailView.as_view(), name='detail'),
+# ]
+
+from django.contrib.auth.views import LoginView,LogoutView
 from django.urls import path
-from .views import list_books, LibraryDetailView
+
 urlpatterns = [
-  path('', list_books, name='index'),
-  path('detail/<int:book_id>', LibraryDetailView.as_view(), name='detail'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
