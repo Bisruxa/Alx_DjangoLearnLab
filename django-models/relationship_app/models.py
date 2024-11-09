122ff53c5cb2a92f5b4129db3d72e046348dd19f
bisruxa <imbisru@gmail.com>
2024-11-09 11:28:44 -0900

    commited

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -29,6 +29,73 @@ class Librarian(models.Model):
     return self.name
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# models.py
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class UserProfile(models.Model):
+    ADMIN = 'Admin'
+    LIBRARIAN = 'Librarian'
+    MEMBER = 'Member'
+
+    ROLE_CHOICES = [
+        (ADMIN, 'Admin'),
+        (LIBRARIAN, 'Librarian'),
+        (MEMBER, 'Member'),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=100, choices=ROLE_CHOICES, default=MEMBER)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.role}'
+
+# Signal to automatically create a UserProfile when a new User is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+# Signal to save the UserProfile whenever the User model is saved
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
+
+
+
+
+
+
+
+
+
 # class UserProfile(models.Model):
 #   ADMIN ='Admin'
 #   LIBRARIAN = 'Librarian'
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -41,7 +41,33 @@ def admin_view(request):
 
 
 
+# views.py
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseForbidden
 
+# Check if the user has the 'Admin' role
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+# Check if the user has the 'Librarian' role
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+# Check if the user has the 'Member' role
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
 
 
 
