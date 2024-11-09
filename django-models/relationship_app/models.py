6a2b2f11486a26a053ba8a8435140217a5be4bb7
bisruxa <imbisru@gmail.com>
2024-11-09 10:33:08 -0900

    commited

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,22 +1,38 @@
 from django.db import models
 
-class Author(models.Model):
-  name = models.CharField(max_length = 100)
-  def __str__(self):
-    return self.name
+# class Author(models.Model):
+#   name = models.CharField(max_length = 100)
+#   def __str__(self):
+#     return self.name
   
-class Book(models.Model):
-  title = models.CharField(max_length =100)
-  author = models.ForeignKey(Author , on_delete = models.CASCADE)
+# class Book(models.Model):
+#   title = models.CharField(max_length =100)
+#   author = models.ForeignKey(Author , on_delete = models.CASCADE)
+#   def __str__(self):
+#     return self.title
+# class Library(models.Model):
+#   name = models.CharField(max_length = 100)
+#   book = models.ManyToManyField(Book)
+#   def __str__(self):  
+#     return self.name
+# class Librarian(models.Model):
+#   name = models.CharField(max_length = 100)
+#   library = models.OneToOneField(Library, on_delete = models.CASCADE)
+#   def __self__(self):
+#     return self.name
+
+
+class UserProfile(models.Model):
+  ADMIN ='Admin'
+  LIBRARIAN = 'Librarian'
+  MEMBER='Member'
+
+  Roles = [
+    (ADMIN,'Admin'),
+    (LIBRARIAN,'Librarian'),
+    (MEMBER,'Member')
+  ]
+  role = models.CharField(max_length = 100,choices=Roles,default=MEMBER)
+  user = models.OneToOneField('auth.User', on_delete = models.CASCADE)
   def __str__(self):
-    return self.title
-class Library(models.Model):
-  name = models.CharField(max_length = 100)
-  book = models.ManyToManyField(Book)
-  def __str__(self):  
-    return self.name
-class Librarian(models.Model):
-  name = models.CharField(max_length = 100)
-  library = models.OneToOneField(Library, on_delete = models.CASCADE)
-  def __self__(self):
-    return self.name
+    return self.role
