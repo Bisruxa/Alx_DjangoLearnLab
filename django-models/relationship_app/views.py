4c90feb70c8d60357ee9e06bdf72104b3d25bd32
bisruxa <imbisru@gmail.com>
2024-11-10 08:40:41 -0900

    admin_view.html have been added

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,19 +1,30 @@
-# from django.shortcuts import render
-# from django.views.generic.detail import DetailView
-# from .models import Book
-# from .models import Library
+from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import Book
+from .models import Library
 
-# def list_books(request):
-#  books =Book.objects.all()
-#  return render(request, 'relationship_app/list_books.html', {'book': books})
-# class LibraryDetailView(DetailView):
-#   model = Book
-#   template_name = 'relationship_app/library_detail.html'
-#   def get_context_data(self, **kwargs):
-#     context = super().get_context_data(**kwargs)
-#     library = self.get_object()
-#     context['name'] = library
-#     return context
+def list_books(request):
+ books =Book.objects.all()
+ return render(request, 'relationship_app/list_books.html', {'book': books})
+class LibraryDetailView(DetailView):
+  model = Book
+  template_name = 'relationship_app/library_detail.html'
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    library = self.get_object()
+    context['name'] = library
+    return context
+  
+
+
+
+
+
+
+
+
+
+  
 
 from django.shortcuts import render
 from django.http import HttpResponse
