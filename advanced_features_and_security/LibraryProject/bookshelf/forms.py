d9b271ad71d876135d05d85df07a1bb9b161d81c
bisruxa <imbisru@gmail.com>
2024-11-15 09:37:50 -0900

    new

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from .models import Book  # Import the Book model
 
 # Model form for Book
-class BookForm(forms.ModelForm):
+class ExampleForm(forms.ModelForm):
     class Meta:
         model = Book  # Link the form to the Book model
         fields = ['title', 'description']  # Include the fields you want in the form
