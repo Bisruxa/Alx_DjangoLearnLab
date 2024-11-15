07f7723a3df6c6cb00723afacda9b0ceb6f5fb07
bisruxa <imbisru@gmail.com>
2024-11-15 09:36:05 -0900

    new

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,1 +1,10 @@
+# bookshelf/forms.py
+from django import forms
+from .models import Book  # Import the Book model
+
+# Model form for Book
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book  # Link the form to the Book model
+        fields = ['title', 'description']  # Include the fields you want in the form
 
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -30,3 +30,19 @@ class CustomUser(AbstractUser):
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email', 'date_of_birth', 'profile_photo']
+# bookshelf/models.py
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+
+    class Meta:
+        permissions = [
+            ("can_create", 'Can create book'),
+            ("can_delete", 'Can delete book'),
+            ("can_edit", 'Can edit book'),
+            ("can_view", 'Can view book'),
+        ]
+
+    def __str__(self):
+        return self.title
