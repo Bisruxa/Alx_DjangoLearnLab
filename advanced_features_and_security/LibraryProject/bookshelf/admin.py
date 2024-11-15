de7d99379aafeb5c3b5527d5c67143666753ca56
bisruxa <imbisru@gmail.com>
2024-11-15 05:14:25 -0900

    name changed

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+from .models import CustomUser
 class UserAdmin(BaseUserAdmin):
   list_display=["username","date_of_birth"]
-admin.site.register( User, UserAdmin)
+admin.site.register( CustomUser, CustomUserAdmin)
