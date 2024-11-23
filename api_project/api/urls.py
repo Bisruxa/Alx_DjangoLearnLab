d6e12cf04e6d0392e3b5b0938a68e3d721d6519b
bisruxa <imbisru@gmail.com>
2024-11-23 10:51:33 -0900

    3

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import BookList
+from .views import BookList,BookViewSet
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register(r'books',BookViewSet,basename='book_all')
 urlpatterns = [
   path("books/", BookList.as_view(),name="book_list"),
+  path('', include(router.urls))
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework.viewsets import ModelViewSet
 class BookList(generics.ListAPIView):
+  queryset = Book.objects.all(ModelViewSet)
+  serializer_class = BookSerializer
+
+class BookViewSet(ModelViewSet):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
