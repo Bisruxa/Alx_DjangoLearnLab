053ffc32cf5ec68ead9e430495ef329402bb03c7
bisruxa <imbisru@gmail.com>
2024-11-29 02:45:37 -0900

    custom view have been created

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,9 +2,39 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Book,Author
 from .serializers import BookSerializer,AuthorSerializer
-class Bookviewset(viewsets.ModelViewSet):
+# class Bookviewset(viewsets.ModelViewSet):
+#   queryset = Book.objects.all()
+#   serializer_class = BookSerializer
+# class Authorviewset(viewsets.ModelViewSet):
+#   queryset = Author.objects.all()
+#   serializer_class = AuthorSerializer
+from rest_framework.generics import (
+    ListAPIView,
+    CreateAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView
+)
+from rest_framework.permissions import IsAuthenticated,AllowAny
+
+class BookListView(ListAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
-class Authorviewset(viewsets.ModelViewSet):
-  queryset = Author.objects.all()
-  serializer_class = AuthorSerializer
+  permission_classes=[AllowAny]
+
+class BookCreateView(CreateAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes=[IsAuthenticated]
+class BookDetailView(RetrieveAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes=[AllowAny]
+class BookUpdateView(UpdateAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes=[IsAuthenticated]
+class BookDeleteView(DestroyAPIView): 
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes=[IsAuthenticated]
