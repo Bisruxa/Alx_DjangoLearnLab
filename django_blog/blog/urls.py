0a694ab106b802f98911b58b8e09d901ca966739
bisruxa <imbisru@gmail.com>
2024-12-05 00:39:03 -0900

    url have been corrected

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,11 +6,11 @@ urlpatterns = [
     path('register/',views.register_view,name='register'),
     path('logout/',views.logout_view,name ='logout'),
     path('profile/', views.profile_view, name='profile'),
-    path('posts/',views.PostListView.as_view(),name = 'post_list'),
+    path('post/',views.PostListView.as_view(),name = 'post_list'),
     # path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-     path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
-     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-     path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
-     path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+     path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
   
 ]
